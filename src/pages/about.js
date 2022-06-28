9243eddb9058b689befe21f1c24c808aa5e333c2
jihun-24k <gi020477@gmail.com>
2022-06-28 13:08:27 +0900

    Feat: Head 컴포넌트 도입

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Head from "../component/head";
 
 const AboutPage = () => {
   return (
     <main>
       <title>자기소개 </title>
+      <Head/>
       <h1>자기 소개 페이지입니다.</h1>
     </main>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Head from "../component/head";
 
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
+      <Head/>
+
       <h1>Home Page</h1>
     </main>
   );
