88da7f0c37fc32969cf8b0980d2d750a2bde62c6
jihun-24k <gi020477@gmail.com>
2022-06-28 13:09:38 +0900

    Feat: test, test-space 페이지 생성

---
+++ src/pages/test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "../component/head";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>자기소개 </title>
+      <Head/>
+      <h1>자기 소개 페이지입니다.</h1>
+    </main>
+  );
+};
+
+export default AboutPage;
