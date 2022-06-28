21a8882a253af9696ac0ad423c5e41436a4563f0
jihun-24k <gi020477@gmail.com>
2022-06-28 16:48:39 +0900

    Bug: 오류

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,29 +1,34 @@
 import { graphql, useStaticQuery } from "gatsby";
- import React from "react";
- import Layout from "../components/layout";
+import React from "react";
+import Layout from "../components/layout";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
  function BlogPage() {
-   const data = useStaticQuery(graphql`
-     query {
-       allFile {
-         nodes {
-           name
-         }
-       }
-     }
-   `);
+    const data = useStaticQuery(graphql`
+    query {
+        allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+          nodes {
+            frontmatter {
+              date(formatString: "MMMM D, YYYY")
+              title
+            }
+            id
+            body
+          }
+        }
+      }
+  `);
 
-   return (
-     <Layout pageTitle="My Blog Posts">
-       <p>My cool posts will go in here</p>
-
-       <ul>
-         {data.allFile.nodes.map((node) => (
-           <li key={node.name}>{node.name}</li>
-         ))}
-       </ul>
-     </Layout>
-   );
- }
-
- export default BlogPage;
+    return (
+    <Layout pageTitle="My Blog Posts">
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
+    </Layout>
+  );
+}
+export default BlogPage;
