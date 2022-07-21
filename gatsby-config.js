caa9b298289b5496dcfc521d61f4535878c338a9
jihun-24k <gi020477@gmail.com>
2022-07-22 04:56:31 +0900

    Feat: first-post 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,14 @@ module.exports = {
       }
     },
     "gatsby-plugin-mdx",
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Noto+Sans+KR\:100,300,400,500,700,900`
+          ],
+        display: "swap",
+      },
+    },
   ],
 }
